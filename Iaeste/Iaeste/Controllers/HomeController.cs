037bf7a8bdce8044ede077de7c15b726c2cc5dd4
superviktor <victor.pryhidko@gmail.com>
2016-02-18 02:06:25 +0200

    photo gallery implemented

---
+++ Iaeste/Iaeste/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Iaeste.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ConferencePhotos()
+        {
+            ViewBag.Images = Directory.EnumerateFiles(Server.MapPath("~/Content/Images/Conference2016"))
+                             .Select(fn => "~/Content/Images/Conference2016/" + Path.GetFileName(fn));
+            return View();
+        }
+
+    }
+}
---
+++ Iaeste/Iaeste/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Iaeste.MvcApplication" Language="C#" %>
